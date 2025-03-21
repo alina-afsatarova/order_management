b856acaf2202e73fb851cb4dc0a6700d985bc16a
alina-afsatarova <alina-aly777@yandex.ru>
2025-03-21 22:14:58 +0300

    Add swagger

--- order_management/api/urls.py
+++ order_management/api/urls.py
@@ -1,5 +1,8 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
+from drf_spectacular.views import (
+    SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
+)
 
 from .views import OrderViewSet
 
@@ -9,5 +12,16 @@ router = DefaultRouter()
 router.register(r'orders', OrderViewSet, basename='orders')
 
 urlpatterns = [
-    path('', include(router.urls))
+    path('', include(router.urls)),
+    path('schema/', SpectacularAPIView.as_view(), name='schema'),
+    path(
+        'docs/',
+        SpectacularSwaggerView.as_view(url_name='schema'),
+        name='swagger-ui'
+    ),
+    path(
+        'schema/redoc/',
+        SpectacularRedocView.as_view(url_name='schema'),
+        name='redoc'
+    ),
 ]
--- order_management/order_management/settings.py
+++ order_management/order_management/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'django_filters',
     'django_bootstrap5',
+    'drf_spectacular',
 
     'orders',
     'api',
@@ -134,3 +135,14 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'Order Management',
+    'DESCRIPTION': '',
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': False
+}
